a93e802f8ab33e423253165d12d44ee498f451b0
Thibault Sainrat <sainrat.thibault@gmail.com>
2019-03-11 12:02:51 +0100

    React convert (materialize)

---
+++ cv/src/js/component/cv/cv.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class CV extends Component {
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default CV;
---
+++ cv/src/js/component/intro/intro.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Intro extends Component {
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default Intro;
